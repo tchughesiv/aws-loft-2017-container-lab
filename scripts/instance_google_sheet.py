44f00b9ae5f277147559f84863d01f8012b52a85
Joseph Callen <jcallen@redhat.com>
2017-07-31 16:53:19 -0400

    Replaced Google Spreadsheet library
    The library was way too slow for any use.

--- scripts/instance_google_sheet.py
+++ scripts/instance_google_sheet.py
@@ -2,37 +2,36 @@
 
 # Used this resources to build this simple script
 # https://boto3.readthedocs.io/en/latest/guide/ec2-example-managing-instances.html
+# https://pygsheets.readthedocs.io/en/latest/
+
+
+# This library below was extremely slow removed for pygsheets
 # http://gspread.readthedocs.io/en/latest/
 # https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
 
 
 from __future__ import print_function
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import pygsheets
 import boto3
-
 
 def main():
     ec2 = boto3.client('ec2')
 
-    filters = [{'Name':'tag:lab_type', 'Values':["loft-lab"]}]
+    filters = [{'Name':'tag:lab_type', 'Values':["loft-lab"],'Name': 'instance-state-name', 'Values': ['running']}]
     instances = ec2.describe_instances(Filters=filters)
-
-
-    scope = ['https://spreadsheets.google.com/feeds']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('/home/jcallen/client_secret.json', scope)
-    client = gspread.authorize(creds)
-
-    sheet = client.open("aws-loft").sheet1
+    gc = pygsheets.authorize(service_file='/home/jcallen/client_secret.json')
 
     row = ["Student ID", "Public URL", "Public IP Address", "Claimed By"]
 
-    sheet.delete_row(1)
-    sheet.insert_row(row, 1)
+    sht = gc.open("aws-loft")
+    wks = sht.worksheet('index', 0)
+
+    wks.update_row(1, values=row)
 
     row_count = 2
 
     for r in instances['Reservations']:
+
         for i in r['Instances']:
             for t in i['Tags']:
                 if t['Key'] == 'Name':
@@ -45,8 +44,9 @@ def main():
             print(i['PublicIpAddress'])
 
             row = [student_id, i['PublicDnsName'], i['PublicIpAddress']]
-            sheet.delete_row(row_count)
-            sheet.insert_row(row, row_count)
+
+            wks.update_row(row_count, values=row)
+
             row_count = row_count + 1
 
 
